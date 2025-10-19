f8227dbbc002e28131ef483cb80d10851df847c7
mohammed saleh albahouth <421108068@qu.edu.sa>
2025-10-19 11:32:40 +0300

    final check of the unit test

--- frontend/test_failing.js
+++
@@ -1,9 +0,0 @@
-// Simple failing JavaScript test
-function testFailingAddition() {
-    if (2 + 2 !== 5) {  // This is wrong!
-        throw new Error("Failing test: 2+2 should equal 5 (this is wrong on purpose)");
-    }
-}
-
-// This will throw an error and fail the test
-testFailingAddition();
