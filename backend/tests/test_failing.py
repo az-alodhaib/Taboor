c6b830567f4a338175f03d16732c5c1091eda7cf
mohammed saleh albahouth <421108068@qu.edu.sa>
2025-10-19 11:26:59 +0300

    test

--- backend/tests/test_failing.py
+++
@@ -1,8 +0,0 @@
-def test_failing_addition():
-    """This test should FAIL - wrong expected result"""
-    assert 1 + 1 == 3  # This is wrong!
-
-def test_failing_string():
-    """This test should FAIL - string mismatch"""
-    name = "Taboor"
-    assert name == "WrongName"  # This will fail
