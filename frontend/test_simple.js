e164b462df01ce20aaff91320663d2ede8777272
mohammed saleh albahouth <421108068@qu.edu.sa>
2025-10-22 10:37:00 +0300

    check javascript syntax and unit test error

--- frontend/test_simple.js
+++ frontend/test_simple.js
@@ -1,27 +1,23 @@
-// syntax error
-// function brokenFunction(
-//     console.log("This function has missing parenthesis"
+syntax error
+function brokenFunction(
+    console.log("This function has missing parenthesis"
 
-// function testAddition() {
-//     let result = 2 + 2;
-//     if (result !== 4) throw new Error("Addition failed");
-//     console.log("Addition test passed!");
-// }
+function testAddition() {
+    let result = 2 + 2;
+    if (result !== 4) throw new Error("Addition failed");
+    console.log("Addition test passed!");
+}
 
-// function testSubtraction() {
-//     let result = 5 - 3;
-//     if (result !== 2) throw new Error("Subtraction failed");
-//     console.log("Subtraction test passed!");
-// }
+function testSubtraction() {
+    let result = 5 - 3;
+    if (result !== 2) throw new Error("Subtraction failed");
+    console.log("Subtraction test passed!");
+}
 
-// // faild unit test
-// function testMultiplication() {
-//     let result = 3 * 3;
-//     if (result !== 10) throw new Error("Multiplication failed");
-//     console.log("This won't print because test fails before here");
-// }
+// faild unit test
+function testMultiplication() {
+    let result = 3 * 3;
+    if (result !== 10) throw new Error("Multiplication failed");
+    console.log("This won't print because test fails before here");
+}
 
-// testAddition();
-// testSubtraction();
-// testMultiplication();
-// console.log("All tests completed!");
