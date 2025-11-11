fb716b0d3466d87bfc7352807edf3d66255a9009
Abdulaziz <az.alodhaib@outlock.com>
2025-11-11 17:42:07 +0300

    this temp work (not finished)

---
+++ frontend/business_index.js
@@ -0,0 +1,15 @@
+// Wait for page to load
+document.addEventListener('DOMContentLoaded', function() {
+    const registerForm = document.getElementById('businesses-register-form');
+    registerForm.addEventListener('submit', async(e) => {
+        e.preventDefault();
+
+        const BusinessName = document.getElementById('name').value;
+        const BusinessEmail = document.getElementById('name').value;
+        const BusinessPhone = document.getElementById('name').value;
+        const BusinessCategory = document.getElementById('name').value;
+        const BusinessAdress = document.getElementById().value;
+        
+
+    });
+});
